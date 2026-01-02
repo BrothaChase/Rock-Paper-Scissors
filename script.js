61ad2cbd4c4918a7339a89678163dc446e3007e5
Chase Brown <chasebrown70131@gmail.com>
2026-01-01 20:17:44 -0600

    created UserChoice function and message prompt

---
+++ script.js
@@ -0,0 +1,23 @@
+function getComputerChoice() {
+  let randomVal = Math.random();
+
+  if (randomVal <= 0.33) {
+    return "rock";
+  }
+  else if (randomVal > 0.33 && randomVal <= 0.66){
+    return "paper";
+  }
+  else {
+    return "scissors";
+  }
+   
+}
+ console.log(getComputerChoice());
+
+ function getUserChoice() {
+     window.prompt("Rock, Paper or Scissors?");
+    }
+    
+    console.log(getUserChoice());
+    
+    
